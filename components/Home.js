de83ea0d13276dcf4b0b05bae03139f9a5f7ac9d
Elian <elian.alphonse@gmail.com>
2026-01-05 12:03:19 +0100

    Mise à jour du composant Home : ajout de la gestion des posts, du composant NewPost et du ProfileCard, et réorganisation du style CSS.

--- components/Home.js
+++ components/Home.js
@@ -1,15 +1,62 @@
-import styles from '../styles/Home.module.css';
+import styles from '../styles/HomePage/Home.module.css';
+import { useEffect, useState } from 'react';
+import { postService } from '../services/postService';
+import Posts from '../components/Posts'
+import NewPost from '../components/newPost'
+import ProfileCard from './ProfileCard';
+import Image from 'next/image'
+import useAuthStore from '../stores/authStore';
 
 function Home() {
+  const [posts, setPosts] = useState([]);
+  const user = useAuthStore((state) => state);
+
+  const fetchPosts = async () => {
+    const data = await postService.getAllPosts();
+    setPosts(data.data);
+    console.log('posts :', data);      
+  };
+
+  useEffect(() => {
+    fetchPosts();
+  }, []);
+
   return (
-    <div>
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-      </main>
-      <div className={styles.div}>
-        
+    <div className={styles.main}>
+      <div className={styles.leftPart}>
+        <div className={styles.logoPart}>
+          <Image
+            src='/favicon.ico.png'
+            width={70}
+            height={70}
+            alt="DarkChirp Logo"
+          />
+        </div>
+        <div className={styles.profilePart}>
+          <ProfileCard profilePicture={user.profilePicture} firstname={user.firstname} username={user.username} />
+        </div>
+      </div>
+      <div className={styles.postsPart}>
+        <h5 className={styles.title}>Home</h5>
+        <div className={styles.newPost}>
+          <NewPost onPostCreated={fetchPosts} />
+        </div>
+        <div className={styles.displayPosts}>
+          {posts.map(post => (
+            <Posts
+              key={post._id}
+              firstname={post.user.firstname}
+              createdAt={post.createdAt}
+              content={post.content}
+              profilePicture={post.user.profilePicture}
+              postId={post._id}
+              onPostDeleted={fetchPosts}
+            />
+          ))}
+        </div>
+      </div>
+      <div className={styles.rightPart}>
+        Trend
       </div>
     </div>
   );
