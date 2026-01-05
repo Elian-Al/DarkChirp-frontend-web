3062e71e84ede15994cdf01a376479a111ac9dd1
Elian <elian.alphonse@gmail.com>
2026-01-05 12:04:42 +0100

    Mise à jour de la configuration Next.js pour autoriser les images de Cloudinary, ajout de nouveaux fichiers SVG pour les avatars par défaut et mise à jour des styles globaux.

--- next.config.js
+++ next.config.js
@@ -2,7 +2,13 @@
 const nextConfig = {
   reactStrictMode: true,
   images: {
-    domains: [],
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'res.cloudinary.com',
+        pathname: '/**',
+      }
+    ]
   },
 };
 
