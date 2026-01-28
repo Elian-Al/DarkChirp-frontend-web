b42fd84d541877a84fbc9e8929ea3b825df3aea5
Elian <elian.alphonse@gmail.com>
2026-01-29 00:08:14 +0100

    Ajout de l'importation de useAuthStore et de l'effet pour hydrater l'état d'authentification lors du chargement du composant

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,15 @@
 import '../styles/globals.css';
 import Head from 'next/head';
+import { useEffect } from 'react';
+import useAuthStore from '../stores/authStore';
 
 function App({ Component, pageProps }) {
+  const hydrate = useAuthStore((state) => state.hydrate);
+
+  useEffect(() => {
+    hydrate();
+  });
+
   return (
     <>
       <Head>
