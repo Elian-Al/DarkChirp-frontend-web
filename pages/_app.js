822aa815cdef8db8f66f37b35b849741d122ee50
Elian <elian.alphonse@gmail.com>
2026-06-07 00:18:59 +0200

    Refactorisation du composant App : suppression du code commenté et amélioration de la structure

--- pages/_app.js
+++ pages/_app.js
@@ -2,32 +2,33 @@ import "../styles/globals.css";
 import Head from "next/head";
 import { useEffect, useState } from "react";
 import useAuthStore from "../stores/authStore";
+import Loading from "../components/UI/Loading";
 
 function App({ Component, pageProps }) {
-    const hydrate = useAuthStore((state) => state.hydrate);
-    const [isReady, setIsReady] = useState(false);
+  const hydrate = useAuthStore((state) => state.hydrate);
+  const [isReady, setIsReady] = useState(false);
 
-    const init = async () => {
-        await hydrate();
-        setIsReady(true);
-    };
+  //   const init = async () => {
+  //     await hydrate();
+  //     setIsReady(true);
+  //   };
 
-    useEffect(() => {
-        init();
-    }, []);
+  //   useEffect(() => {
+  //     init();
+  //   }, []);
 
-    if (!isReady) {
-        return <div>Chargement...</div>;
-    }
+  //   if (!isReady) {
+  //     return <Loading>Chargement...</Loading>;
+  //   }
 
-    return (
-        <>
-            <Head>
-                <title>DarkChirp</title>
-            </Head>
-            <Component {...pageProps} />
-        </>
-    );
+  return (
+    <>
+      <Head>
+        <title>DarkChirp</title>
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
 
 export default App;
