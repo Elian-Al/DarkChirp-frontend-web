224f9902f08d92715b54367d781803804ba49697
Elian <elian.alphonse@gmail.com>
2026-06-07 00:19:09 +0200

    Ajout du composant Loading dans le composant UI et création du fichier CSS associé.

--- components/UI.js
+++ components/UI.js
@@ -1,26 +1,37 @@
 import Modal from "./UI/Modal";
 import Button from "./UI/Button";
+import Loading from "./UI/Loading";
 import { useState } from "react";
 
 function UI() {
-    const [isModalOpen, setIsModalOpen] = useState(false);
+  const [isModalOpen, setIsModalOpen] = useState(false);
 
-    const handleOpenModal = () => {
-        setIsModalOpen(true);
-    };
+  const handleOpenModal = () => {
+    setIsModalOpen(true);
+  };
 
-    const handleCloseModal = () => {
-        setIsModalOpen(false);
-    };
-    return (
-        <div>
-            <h1>UI Page</h1>
-            <Button onClick={handleOpenModal}>Test Modal</Button>
-            <Modal isOpen={isModalOpen} onClose={handleCloseModal}>
-                Test
-            </Modal>
-        </div>
-    );
+  const handleCloseModal = () => {
+    setIsModalOpen(false);
+  };
+  return (
+    <div
+      style={{
+        border: "1px solid red",
+        minHeight: "100vh",
+        display: "flex",
+        "flex-direction": "column",
+        "align-items": "center",
+        gap: 30,
+      }}
+    >
+      <h1>UI Page</h1>
+      <Button onClick={handleOpenModal}>Test Modal</Button>
+      <Modal isOpen={isModalOpen} onClose={handleCloseModal}>
+        Test
+      </Modal>
+      <Loading>Chargement en cours...</Loading>
+    </div>
+  );
 }
 
 export default UI;
