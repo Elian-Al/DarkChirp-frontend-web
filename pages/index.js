b1bab3bc1c668b8a9c690e8663d51a894b13114d
Elian <elian.alphonse@gmail.com>
2026-06-11 14:25:26 +0200

    Nettoyage du code : suppression des lignes commentées liées à l'authentification dans le composant Index

--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,14 @@
-import Login from '../components/login';
-import { useRouter } from 'next/router';
-import useAuthStore from '../stores/authStore';
+import Login from "../components/login";
+import { useRouter } from "next/router";
+import useAuthStore from "../stores/authStore";
 
 function Index() {
-  const user = useAuthStore((state) => state);
-  const router = useRouter();
+  // const user = useAuthStore((state) => state);
+  // const router = useRouter();
 
-  if (user.isAuthenticated) {
-    router.push('/Home');
-  };
-
+  // if (user.isAuthenticated) {
+  //   router.push('/Home');
+  // };
 
   return <Login />;
 }
